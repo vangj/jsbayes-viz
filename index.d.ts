338e08cebe6adbb582043d686cfafacfc0e255c8
Jee Vang, Ph.D <vangjee@gmail.com>
2018-11-14 12:53:55 -0500

    add more props

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,29 @@
 interface VGraph {
-
+  nodes: Array<VNode>;
+  edges: Array<VEdge>;
+  addNode(id: string, label: string, values: Array<string>, probs: Array<number>): VNode;
+  addEdge(parent: string, child: string): VEdge;
+  node(id: string): VNode;
+  edge(id1: string, id2: string): VEdge;
+}
+interface VNode {
+  id: string;
+  label: string;
+  values: Array<string>;
+  probs: Array<number>;
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+}
+interface VEdge {
+  parent: string;
+  child: string;
 }
 interface DrawOptions {
   id: string;
-  width: number;
-  height: number;
+  width: number | any;
+  height: number | any;
   graph: VGraph;
   samples: number;
 }
